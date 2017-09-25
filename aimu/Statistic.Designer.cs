a6f778e8f4b35c28eb73f9c33951d1683bf05152
wynet321@163.com <wynet321@163.com>
2017-09-25 21:57:51 +0800

    Add statistic form

---
+++ aimu/Statistic.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace aimu
+{
+    partial class Statistic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.dateTimePickerStartDate = new System.Windows.Forms.DateTimePicker();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.textBoxPartnerName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxChannel = new System.Windows.Forms.ComboBox();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.labelConsultant = new System.Windows.Forms.Label();
+            this.textBoxConsultant = new System.Windows.Forms.TextBox();
+            this.dataGridViewCustomers = new System.Windows.Forms.DataGridView();
+            this.dateTimePickerEndDate = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(201, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "终止日期";
+            this.label1.Visible = false;
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(846, 7);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(78, 25);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "查询";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Visible = false;
+            // 
+            // dateTimePickerStartDate
+            // 
+            this.dateTimePickerStartDate.Enabled = false;
+            this.dateTimePickerStartDate.Location = new System.Drawing.Point(71, 9);
+            this.dateTimePickerStartDate.Name = "dateTimePickerStartDate";
+            this.dateTimePickerStartDate.Size = new System.Drawing.Size(124, 21);
+            this.dateTimePickerStartDate.TabIndex = 10;
+            this.dateTimePickerStartDate.Value = new System.DateTime(2017, 4, 17, 0, 0, 0, 0);
+            this.dateTimePickerStartDate.Visible = true;
+            
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePickerEndDate);
+            this.splitContainer1.Panel1.Controls.Add(this.buttonSearch);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxPartnerName);
+            this.splitContainer1.Panel1.Controls.Add(this.label3);
+            this.splitContainer1.Panel1.Controls.Add(this.dateTimePickerStartDate);
+            this.splitContainer1.Panel1.Controls.Add(this.comboBoxChannel);
+            this.splitContainer1.Panel1.Controls.Add(this.labelStatus);
+            this.splitContainer1.Panel1.Controls.Add(this.labelConsultant);
+            this.splitContainer1.Panel1.Controls.Add(this.textBoxConsultant);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.dataGridViewCustomers);
+            this.splitContainer1.Size = new System.Drawing.Size(1084, 562);
+            this.splitContainer1.SplitterDistance = 37;
+            this.splitContainer1.TabIndex = 12;
+            // 
+            // textBoxPartnerName
+            // 
+            this.textBoxPartnerName.Location = new System.Drawing.Point(745, 9);
+            this.textBoxPartnerName.Name = "textBoxPartnerName";
+            this.textBoxPartnerName.Size = new System.Drawing.Size(86, 21);
+            this.textBoxPartnerName.TabIndex = 20;
+            this.textBoxPartnerName.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(686, 13);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "异业名称";
+            this.label3.Visible = false;
+            // 
+            // comboBoxChannel
+            // 
+            this.comboBoxChannel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxChannel.FormattingEnabled = true;
+            this.comboBoxChannel.Location = new System.Drawing.Point(564, 9);
+            this.comboBoxChannel.Name = "comboBoxChannel";
+            this.comboBoxChannel.Size = new System.Drawing.Size(116, 20);
+            this.comboBoxChannel.TabIndex = 16;
+            this.comboBoxChannel.SelectedIndexChanged += new System.EventHandler(this.comboBoxChannel_SelectedIndexChanged);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(529, 13);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(29, 12);
+            this.labelStatus.TabIndex = 15;
+            this.labelStatus.Text = "渠道";
+            // 
+            // labelConsultant
+            // 
+            this.labelConsultant.AutoSize = true;
+            this.labelConsultant.Location = new System.Drawing.Point(390, 13);
+            this.labelConsultant.Name = "labelConsultant";
+            this.labelConsultant.Size = new System.Drawing.Size(41, 12);
+            this.labelConsultant.TabIndex = 13;
+            this.labelConsultant.Text = "礼服师";
+            this.labelConsultant.Visible = false;
+            // 
+            // textBoxConsultant
+            // 
+            this.textBoxConsultant.Location = new System.Drawing.Point(437, 9);
+            this.textBoxConsultant.Name = "textBoxConsultant";
+            this.textBoxConsultant.Size = new System.Drawing.Size(86, 21);
+            this.textBoxConsultant.TabIndex = 14;
+            this.textBoxConsultant.Visible = false;
+            // 
+            // dataGridViewCustomers
+            // 
+            this.dataGridViewCustomers.AllowUserToAddRows = false;
+            this.dataGridViewCustomers.AllowUserToDeleteRows = false;
+            this.dataGridViewCustomers.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCustomers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCustomers.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewCustomers.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewCustomers.MultiSelect = false;
+            this.dataGridViewCustomers.Name = "dataGridViewCustomers";
+            this.dataGridViewCustomers.ReadOnly = true;
+            this.dataGridViewCustomers.RowTemplate.Height = 23;
+            this.dataGridViewCustomers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewCustomers.Size = new System.Drawing.Size(1084, 521);
+            this.dataGridViewCustomers.TabIndex = 11;
+            this.dataGridViewCustomers.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridViewCustomers_CellMouseDoubleClick);
+            // 
+            // dateTimePickerEndDate
+            // 
+            this.dateTimePickerEndDate.Enabled = false;
+            this.dateTimePickerEndDate.Location = new System.Drawing.Point(260, 9);
+            this.dateTimePickerEndDate.Name = "dateTimePickerEndDate";
+            this.dateTimePickerEndDate.Size = new System.Drawing.Size(124, 21);
+            this.dateTimePickerEndDate.TabIndex = 22;
+            this.dateTimePickerEndDate.Value = new System.DateTime(2017, 4, 17, 0, 0, 0, 0);
+            this.dateTimePickerEndDate.Visible = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "起始日期";
+            this.label2.Visible = false;
+            // 
+            // Statistic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 562);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Statistic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "统计";
+            this.Load += new System.EventHandler(this.Statistic_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCustomers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.DateTimePicker dateTimePickerStartDate;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ComboBox comboBoxChannel;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.Label labelConsultant;
+        private System.Windows.Forms.TextBox textBoxConsultant;
+        private System.Windows.Forms.DataGridView dataGridViewCustomers;
+        private System.Windows.Forms.TextBox textBoxPartnerName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerEndDate;
+    }
+}
---
+++ aimu/Statistic.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aimu
+{
+    public partial class Statistic : Form
+    {
+        public Statistic()
+        {
+            InitializeComponent();
+        }
+
+        private void changeDataGridView()
+        {
+            if (dataGridViewCustomers.Columns["orderId"] != null)
+                dataGridViewCustomers.Columns["orderId"].HeaderText = "订单编号";
+            if (dataGridViewCustomers.Columns["memo"] != null)
+                dataGridViewCustomers.Columns["memo"].HeaderText = "备注";
+            if (dataGridViewCustomers.Columns["bridename"] != null)
+                dataGridViewCustomers.Columns["bridename"].HeaderText = "姓名";
+            if (dataGridViewCustomers.Columns["bridecontact"] != null)
+                dataGridViewCustomers.Columns["bridecontact"].HeaderText = "联系方式";
+        }
+
+        private void dataGridViewCustomers_CellMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridViewRow row = this.dataGridViewCustomers.Rows[e.RowIndex];
+            Form bt = new OrderStandard(row.Cells["orderId"].Value.ToString());
+            bt.ShowDialog();
+        }
+
+        private void Statistic_Load(object sender, EventArgs e)
+        {
+            //comboBoxChannel.DataSource = ReadData.getOrderStatus(Sharevariables.getUserLevel()).DefaultView;
+            //comboBoxChannel.DisplayMember = "name";
+            //comboBoxChannel.SelectedIndex = 0;
+        }
+        
+        private void comboBoxChannel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = ReadData.getOrderByStatus(int.Parse(((DataView)comboBoxChannel.DataSource).Table.Rows[comboBoxChannel.SelectedIndex].ItemArray[0].ToString()));
+            dataGridViewCustomers.DataSource = dt;
+            changeDataGridView();
+        }
+
+
+    }
+}
